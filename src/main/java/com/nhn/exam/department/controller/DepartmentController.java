e6a6a93c93013ce7eefad38fa8f826f23e1e5e26
LSJ <devel0624@gmail.com>
2022-12-15 10:25:30 +0900

    DepartmentController PostMapping

--- src/main/java/com/nhn/exam/department/controller/DepartmentController.java
+++ src/main/java/com/nhn/exam/department/controller/DepartmentController.java
@@ -1,8 +1,11 @@
 package com.nhn.exam.department.controller;
 
+import com.nhn.exam.department.domain.model.projection.DepartmentProjection;
 import com.nhn.exam.department.domain.model.request.DepartmentRegisterRequest;
+import com.nhn.exam.department.service.DepartmentService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/department")
 public class DepartmentController {
 
-  @PostMapping
-  public void registerDepartment(@Valid @RequestBody DepartmentRegisterRequest request) {
+  // TODO 조직 추가 컨트롤러
 
+  private final DepartmentService departmentService;
+
+  public DepartmentController(DepartmentService departmentService) {
+    this.departmentService = departmentService;
+  }
+
+  @PostMapping
+  public DepartmentProjection registerDepartment(
+      @Valid @RequestBody DepartmentRegisterRequest request, BindingResult result) {
+
+    // TODO 02 발리데이션 에러 처리
+    if (result.hasErrors()) {
+      throw new RuntimeException("Validation Error");
+    }
+
+    DepartmentProjection projection = departmentService.registerDepartment(request);
+
+    return projection;
   }
 }
